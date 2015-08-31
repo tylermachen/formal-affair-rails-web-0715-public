b4351a3df03617668761d3320f33a7616cbcd0e5
Tyler Machen <tylermachen@gmail.com>
2015-08-31 18:02:34 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 FormalAffairRails::Application.routes.draw do
   resources :babies,    only: [:new, :create, :edit, :update]
   resources :concretes, only: [:new, :create, :edit, :update]
-  resource :searches, only: [:new, :create]
+  resource  :searches,  only: [:new, :create]
 end
